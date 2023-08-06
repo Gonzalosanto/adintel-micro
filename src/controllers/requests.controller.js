2138a49b132f3eaae06426f0b863075944ffdd8f
gonzalosanto <sancruzgon@gmail.com>
2023-08-06 15:26:55 -0300

    WIP: Refactor of request controller to optimize and finish pending implementations

--- src/controllers/requests.controller.js
+++ src/controllers/requests.controller.js
@@ -1,32 +1,40 @@
-import { saveDebug, saveError, saveChain, saveResponse } from "../DAOs/fs/index.js";
-import { info,error, warning, debug } from '../middlewares/logger/index.js';
-import { getVASTTagURI,handleBrokenResponse, handleErrorResponse, handleSuccesfulResponse, buildHeaders, baseFrom, addIfXMLResponse } from "../utils/http.js";
+import { info,error, warning, debug, log } from '../middlewares/logger/index.js';
+import { getVASTTagURI,handleBrokenResponse, handleErrorResponse, handleSuccesfulResponse, addIfXMLResponse } from "../utils/http.js";
 import fetch from "node-fetch";
 import { trigger } from "../utils/impressions.utils.js";
 
-//Chaining requests, async function to get a chain
-const getRequest = async (req,res,urlWithMacros) => {
-    const setRequestHeaders = (searchParams) => {
-        const params = Object.fromEntries(searchParams)
-        const headers = {
-            'Connection': 'keep-alive',
-            'Access-Control-Allow-Origin':'*',
-            'Access-Control-Allow-Credentials':true,
-            'Accept': `*/*`,
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'X-Forwarded-For':params.uip || '',
-            'User-Agent': params.ua || '',
-        }
-        return headers;
-    }
-    const controller = new AbortController()
-    const timeout = setTimeout(()=>controller.abort(), 5000);
+//SET state machine to handle every request state during loop life cycle
+// const stateMachine = {
+//     initial: 'start',
+//     states: {
+//         start: {
+//             on: {
+//                 RESPONSE_200: 'success',
+//                 RESPONSE_ERROR: 'error',
+//                 RESPONSE_BROKEN: 'broken',
+//                 RESPONSE_CRITICAL_ERROR: 'criticalError'
+//             }
+//         },
+//         success: {
+//             onEntry: 'handleSuccess'
+//         },
+//         error: {
+//             onEntry: 'handleError'
+//         },
+//         broken: {
+//             onEntry: 'handleBroken'
+//         },
+//         criticalError: {
+//             onEntry: 'handleCriticalError'
+//         }
+//     }
+// }
+
+const getRequest = async (urlWithMacros) => {
     let url = setCachebuster(urlWithMacros);
     const params = new URL(url).searchParams;
     const reqHeaders = setRequestHeaders(params);
+    const timeout = abortHandler(5000)
     let retries = 0;
     let cookies = [];
     let eventsChain = {
@@ -38,7 +46,6 @@ const getRequest = async (req,res,urlWithMacros) => {
         isCriticalError: false,
         isCommonError: false,
     }
-    let data = {};
     while ((!eventsChain.isBroken && !eventsChain.isCriticalError) && retries <= 5){
         try {
             let options = {
@@ -48,31 +55,54 @@ const getRequest = async (req,res,urlWithMacros) => {
             }
             const response = await fetch(url, options);
             cookies = response.headers.get('Set-Cookie')?.split(';')[0] || '';
-            data = await dataToLog(reqHeaders, response, url);
+            const data = await dataToLog(reqHeaders, response, url);
             eventsChain.previousURL = url
             eventsChain.eventChain.push(data);
             if(response.status == 200){
-                options.headers['Cookies'] = cookies
-                eventsChain.XMLChain = addIfXMLResponse(data.body, eventsChain.XMLChain);
-                eventsChain = handleResponse(data.body, eventsChain);
-                // console.log(data.body)
-                url = handleURLAfterResponse(data.body, eventsChain);
-            }
+                url = chainRequest(response, eventsChain, cookies);
+            } else { 
+                break;
+             }
             retries++;
             clearTimeout(timeout)
         } catch (err) {
-            //Try to make request and handle any error that comes out
-            retries++;
-            data.error = err;
-            //error(`${new Date().toISOString()} -/- ${err}`)
+            error(`${new Date().toISOString()} => ${err}`)
             eventsChain.isCriticalError = true;
-            //eventsChain.eventChain.push(data);
             clearTimeout(timeout)
         }
     }
-    if(eventsChain.isSuccesful) {
-        eventsChain = await handleSuccesfulChain(eventsChain)
-    };
+    if(eventsChain.isSuccesful) {eventsChain = await handleSuccesfulChain(eventsChain)};
+    return eventsChain
+}
+
+const chainRequest = (response, eventsChain, cookies) => {
+    options.headers['Cookies'] = cookies;
+    eventsChain.XMLChain = addIfXMLResponse(response, eventsChain.XMLChain);
+    eventsChain = handleResponse(response, eventsChain);
+    return handleURLAfterResponse(response, eventsChain);
+}
+
+const abortHandler = (timeoutMs)=>{    
+    const controller = new AbortController()
+    const timeout = setTimeout(()=>controller.abort(), timeoutMs);
+    return timeout;
+}
+
+const setRequestHeaders = (searchParams) => {
+    const params = Object.fromEntries(searchParams)
+    const headers = {
+        'Connection': 'keep-alive',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Credentials':true,
+        'Accept': `*/*`,
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'X-Forwarded-For':params.uip || '',
+        'User-Agent': params.ua || '',
+    }
+    return headers;
 }
 
 const setCachebuster = (url) => {
@@ -95,9 +125,9 @@ const handleURLAfterResponse = (data, options) => {
 }
 
 const handleSuccesfulChain = async (chain) => {
-    console.log(`${new Date().toISOString()} --- Cadena Exitosa`)
     const res = await trigger(chain.XMLChain)
     chain.eventChain.push(JSON.stringify(res))
+    console.log(`${new Date().toISOString()} --- Cadena Exitosa`)
     return chain
 }
 
