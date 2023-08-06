f411f61c41c6d6c66d9b59f9731b63401215ce71
gonzalosanto <sancruzgon@gmail.com>
2023-08-06 15:19:23 -0300

    REFACTOR: extracted promiseHandler and trycatch block moved

--- src/controllers/concurrency.js
+++ src/controllers/concurrency.js
@@ -1,34 +1,45 @@
 import { getRequest } from "./requests.controller.js";
 import { GetMacrosLength } from '../services/macros.service.js'
-import {urlsToRequest} from '../utils/builder.utils.js'
+import { urlsToRequest } from '../utils/builder.utils.js'
+import { isValidURL } from "../utils/http.js";
+import { error, log } from "../middlewares/logger/index.js";
 
 const HOUR = 60 * 60 * 1000;
 
 export const runRequestsConcurrently = async (concurrency, hours) => {
     let startTime = Date.now();
-    const promises = [];
     let index = 0;
     let begin = 0;
     const limit = Number.parseInt(concurrency);
     const length = await GetMacrosLength();
     let urls;
-    while ((Date.now() - startTime) < (hours * HOUR)) {
+
+    const handlePromises = async (promises) => {
         try {
-            urls = await urlsToRequest(begin, limit)
-            if(index >= length) {
-                begin = 0; index=0;
-            }
-            for (let i = 0; promises.length < concurrency && index < length; i++) {
-                promises.push(getRequest('','',urls[i]))
-            }
-            await Promise.all(promises);
-            index+=urls.length;
-            begin += limit;
-            promises.length = 0;
+            await Promise.allSettled(promises);
         } catch (error) {
-            console.log("Process exited with error code: F")
-            throw new Error(error)
+            throw new Error(error);
         }
-    }
-    console.log('Loop exited')
-}
+    };
+
+    try {
+        while ((Date.now() - startTime) < (hours * HOUR)) {
+            urls = await urlsToRequest(begin, limit);
+            if (index >= length) {
+                begin = 0;
+                index = 0;
+            }
+            const promises = [];
+            for (let i = 0; promises.length < concurrency && index < length; i++) {
+                if (isValidURL(urls[i])) promises.push(getRequest(urls[i]));
+            }
+            await handlePromises(promises);
+            index += urls.length;
+            begin += limit;
+        }
+    } catch (err) {
+        log("LOOP HAD AN EXCEPTION")
+        error(err) 
+    }    
+    log('Loop finished');
+};
