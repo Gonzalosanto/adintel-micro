6de7208072d7c356da1380797702759fd124fa44
gonzalosanto <sancruzgon@gmail.com>
2023-08-10 17:28:52 -0300

    OK: Se cambian referencias a valores widt y height

--- src/utils/builder.utils.js
+++ src/utils/builder.utils.js
@@ -1,6 +1,6 @@
 import {GetMacros} from '../services/macros.service.js';
-import {BASE_URL, DEVICE_CATEGORY, VAST_VERSION, } from '../../config/index.js';
-const urlBuilder = (macros, id) => {return `${BASE_URL}/?width=${macros.width}&height=${macros.height}&cb=&ua=${macros.ua}&uip=${macros.uip}&app_name=${macros.app_name}&app_bundle=${macros.app_bundle}&device_model=&device_make=&device_category=${DEVICE_CATEGORY}&app_store_url=${encodeURIComponent(macros.app_store_url)}&device_id=${macros.device_id || ''}&vast_version=${VAST_VERSION}&aid=${id}`;}
+import {WIDTH, HEIGHT, BASE_URL, DEVICE_CATEGORY, VAST_VERSION, } from '../../config/index.js';
+const urlBuilder = (macros, id) => {return `${BASE_URL}/?width=${WIDTH}&height=${HEIGHT}&cb=&ua=${macros.ua}&uip=${macros.uip}&app_name=${macros.app_name}&app_bundle=${macros.app_bundle}&device_model=&device_make=&device_category=${DEVICE_CATEGORY}&app_store_url=${encodeURIComponent(macros.app_store_url)}&device_id=${macros.device_id || ''}&vast_version=${VAST_VERSION}&aid=${id}`;}
 const getDataToBuild = async (skip, limit) => {return GetMacros(skip, limit)}
 
 const processURLsToRequest = async (data) => {
