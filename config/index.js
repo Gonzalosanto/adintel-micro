810e8a69a61fe052c9c337415a6ed1eb492f0566
gonzalosanto <sancruzgon@gmail.com>
2023-08-10 17:28:13 -0300

    OK: se agregan constantes Width y Height

--- config/index.js
+++ config/index.js
@@ -2,8 +2,10 @@ import 'dotenv/config';
 
 const PORT = process.env.PORT || 8080;
 const DEVICE_CATEGORY = 4
+const WIDTH = 1920
+const HEIGHT = 1080
 const VAST_VERSION = '2.0'
 const AID = 835805;
 const BASE_URL = 'http://s.adtelligent.com';
 
-export { BASE_URL, AID, PORT, VAST_VERSION, DEVICE_CATEGORY };
+export { WIDTH, HEIGHT ,BASE_URL, AID, PORT, VAST_VERSION, DEVICE_CATEGORY };
