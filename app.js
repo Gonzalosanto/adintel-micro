6ce87bed9e16277aefde53521812c8a8395f4c82
gonzalosanto <sancruzgon@gmail.com>
2023-08-28 19:22:16 -0300

    Connecting to DB and sync

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ import express from 'express';
 const app = express();
 import 'dotenv/config'
 import { router } from "./src/routes/index.js"
+import {client} from './src/db/mariadb.js'
+import {AppBundle, AppName} from './src/models/relational/index.js'
 
 app.use(router)
+
+client.sync() //Connects with mariadb server and sync imported models
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server is listening on : " + process.env.PORT)
 })
-
-/**
- * Gets info from 
- * @param {String} filename filepath of CSV file
- * @returns A boolean that represents if the file was read before to get data.
- */
-const checkIfFileWasRead = (filename) => {
-    //consulta a la base de datos retorna el nombre del archivo leido y algun flag unico
-    return true
-}
