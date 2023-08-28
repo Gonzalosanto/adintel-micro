3d8b79819c0b2f7af30ebbd6f0d796dbd540ead5
gonzalosanto <sancruzgon@gmail.com>
2023-08-28 19:20:17 -0300

    Models index

--- src/models/relational/index.js
+++ src/models/relational/index.js
@@ -1,10 +1,3 @@
-import { Sequelize } from "sequelize";
-//const db_name = 'VAST_DB'
-//export const client = new Sequelize(`mariadb://localhost:/${db_name}`)
-
-const db = new Sequelize('vast_db','','',{
-    host: 'localhost',
-    dialect: 'mariadb',
-    //logging: false,
-});
-export default db;
+import {AppBundle} from './ApplicationBundle.model.js'
+import {AppName} from './ApplicationName.model.js'
+export {AppBundle,AppName};
