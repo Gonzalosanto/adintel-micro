667d4629039b2802aba8680e048e4035ee998269
gonzalosanto <sancruzgon@gmail.com>
2023-08-29 14:21:31 -0300

    test DB creation

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 import 'dotenv/config'
 import { router } from "./src/routes/index.js"
 import {client} from './src/db/mariadb.js'
-import {AppBundle, AppName} from './src/models/relational/index.js'
+import { } from './src/models/relational/index.js'
 
 app.use(router)
 
-client.sync() //Connects with mariadb server and sync imported models
+client.sync({force:true}) //Connects with mariadb server and sync imported models
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server is listening on : " + process.env.PORT)
