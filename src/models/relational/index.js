4ba04e1c9fd934030b96b8266bb10eead1d4374d
gonzalosanto <sancruzgon@gmail.com>
2023-08-29 14:12:01 -0300

    File with relationships btwn tbls

--- src/models/relational/index.js
+++ src/models/relational/index.js
@@ -1,3 +1,15 @@
 import {AppBundle} from './ApplicationBundle.model.js'
 import {AppName} from './ApplicationName.model.js'
-export {AppBundle,AppName};
+
+//TODO: add every model and associations.
+AppBundle.hasOne(AppName,{
+    foreignkey: 'name',
+});
+AppName.hasOne(AppStore,{
+    foreignkey: 'store'
+});
+AppName.belongsTo(AppBundle,{
+    foreignkey: 'bundle',
+    target_key: 'name'
+});
+export {AppName, AppBundle}
