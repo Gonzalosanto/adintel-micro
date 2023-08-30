72cccd7f4ae71671192737c3c469c06a22d90375
gonzalosanto <sancruzgon@gmail.com>
2023-08-30 20:15:44 -0300

    ADD: filter existing values

--- src/services/vast.service.js
+++ src/services/vast.service.js
@@ -2,27 +2,40 @@ import { db_client } from '../db/mariadb.js'
 import {AppName, AppBundle, AppStore, OperativeSystem, DeviceId, UserAgent, UserIP } from '../models/relational/index.js'
 
 
-export const BulkInsertAppNames = async (values)=>{ return  AppName.bulkCreate(values)};
+export const BulkInsertAppNames = async (values)=>{
+    values = filterRepeatedValues(values, AppName) 
+    return  AppName.bulkCreate(values)};
 
-export const BulkInsertAppBundle = async (values)=>{ return  AppBundle.bulkCreate(values)};
+export const BulkInsertAppBundle = async (values)=>{
+    values = filterRepeatedValues(values, AppBundle) 
+    return  AppBundle.bulkCreate(values)};
 
-export const BulkInsertAppStore = async (values)=>{ return  AppStore.bulkCreate(values)};
+export const BulkInsertAppStore = async (values)=>{
+    values = filterRepeatedValues(values, AppStore) 
+    return  AppStore.bulkCreate(values)};
 
-export const BulkInsertOS = async (values)=>{ return  OperativeSystem.bulkCreate(values)};
+export const BulkInsertOS = async (values)=>{
+    values = filterRepeatedValues(values, OperativeSystem) 
+    return  OperativeSystem.bulkCreate(values)};
 
-export const BulkInsertDeviceID = async (values)=>{ return  DeviceId.bulkCreate(values)};
+export const BulkInsertDeviceID = async (values)=>{
+    values = filterRepeatedValues(values, DeviceId) 
+    return  DeviceId.bulkCreate(values)};
 
-export const BulkInsertUserAgent = async (values)=>{ return  UserAgent.bulkCreate(values)};
+export const BulkInsertUserAgent = async (values)=>{
+    values = filterRepeatedValues(values, UserAgent) 
+    return  UserAgent.bulkCreate(values)};
 
-export const BulkInsertUserIP = async (values)=>{ return  UserIP.bulkCreate(values)};
-
-export const BulkInsertData = async () => { 
-    //TODO: This function should insert data according to associations defined in models.
-    
+export const BulkInsertUserIP = async (values)=>{
+    values = filterRepeatedValues(values, 'uip', UserIP)
+    return UserIP.bulkCreate(values)
+};
+const filterRepeatedValues = async (values, key, model) => {
+    const existingValues = await SelectAll(null, model);
+    const existingValuesSet = new Set(existingValues.map(value => value[key]));
+    return values.filter(value => {!existingValuesSet.has(value[key])});
 }
-
 export const SelectAll = async (attributes, model) => {
     if(attributes) return model.findAll(attributes)
     return model.findAll()
 };
-
