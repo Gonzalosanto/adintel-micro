a64a7600cf1d443b31a2947323fab3deb8870baa
gonzalosanto <sancruzgon@gmail.com>
2023-08-30 13:26:51 -0300

    FIX: standard changes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express';
 const app = express();
 import 'dotenv/config'
 import { router } from "./src/routes/index.js"
-import {client} from './src/db/mariadb.js'
+import {db_client} from './src/db/mariadb.js'
 import { } from './src/models/relational/index.js'
 
 app.use(router)
 
-client.sync({force:true}) //Connects with mariadb server and sync imported models
+db_client.sync() //Connects with mariadb server and sync imported models
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server is listening on : " + process.env.PORT)
