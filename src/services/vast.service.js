23b97b63eb3f53bc5442b9ee25276251df41660a
Apu19 <calertios12@gmail.com>
2023-08-31 14:24:58 -0600

    FIX: Awaits faltantes, guard clause

--- src/services/vast.service.js
+++ src/services/vast.service.js
@@ -1,37 +1,38 @@
-import { db_client } from '../db/mariadb.js'
+// import { db_client } from '../db/mariadb.js'
 import {AppName, AppBundle, AppStore, OperativeSystem, DeviceId, UserAgent, UserIP } from '../models/relational/index.js'
 
-
 export const BulkInsertAppNames = async (values)=>{
-    values = filterRepeatedValues(values,'name', AppName) 
+    values = await filterRepeatedValues(values,'name', AppName) 
+    console.log(values);
     return  AppName.bulkCreate(values)};
 
 export const BulkInsertAppBundle = async (values)=>{
-    values = filterRepeatedValues(values,'bundle', AppBundle) 
+    values = await filterRepeatedValues(values,'bundle', AppBundle) 
     return  AppBundle.bulkCreate(values)};
 
 export const BulkInsertAppStore = async (values)=>{
-    values = filterRepeatedValues(values,'store', AppStore) 
+    values = await filterRepeatedValues(values,'store', AppStore) 
     return  AppStore.bulkCreate(values)};
 
 export const BulkInsertOS = async (values)=>{
-    values = filterRepeatedValues(values,'os', OperativeSystem) 
+    values = await filterRepeatedValues(values,'os', OperativeSystem) 
     return  OperativeSystem.bulkCreate(values)};
 
 export const BulkInsertDeviceID = async (values)=>{
-    values = filterRepeatedValues(values,'deviceid', DeviceId) 
+    values = await filterRepeatedValues(values,'deviceid', DeviceId) 
     return  DeviceId.bulkCreate(values)};
 
 export const BulkInsertUserAgent = async (values)=>{
-    values = filterRepeatedValues(values,'ua', UserAgent) 
+    values = await filterRepeatedValues(values,'ua', UserAgent) 
     return  UserAgent.bulkCreate(values)};
 
 export const BulkInsertUserIP = async (values)=>{
-    values = filterRepeatedValues(values, 'uip', UserIP)
+    values = await filterRepeatedValues(values, 'uip', UserIP)
     return UserIP.bulkCreate(values)
 };
 const filterRepeatedValues = async (values, key, model) => {
     const existingValues = await SelectAll(null, model);
+    if(existingValues.length == 0) return values;
     const existingValuesSet = new Set(existingValues.map(value => value[key]));
     return values.filter(value => {!existingValuesSet.has(value[key])});
 }
