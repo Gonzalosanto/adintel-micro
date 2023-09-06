cd6d1052b3514544278655964d59c7c9f57e0092
gonzalosanto <sancruzgon@gmail.com>
2023-09-06 14:34:49 -0300

    FIX: async loading fixed / code format

--- src/services/vast.service.js
+++ src/services/vast.service.js
@@ -4,27 +4,33 @@ import {AppName, AppBundle, AppStore, OperativeSystem, DeviceId, UserAgent, User
 export const BulkInsertAppNames = async (values)=>{
     values = await filterRepeatedValues(values,'name', AppName) 
     console.log(values);
-    return  AppName.bulkCreate(values)};
+    return  AppName.bulkCreate(values)
+};
 
 export const BulkInsertAppBundle = async (values)=>{
     values = await filterRepeatedValues(values,'bundle', AppBundle) 
-    return  AppBundle.bulkCreate(values)};
+    return  AppBundle.bulkCreate(values)
+};
 
 export const BulkInsertAppStore = async (values)=>{
     values = await filterRepeatedValues(values,'store', AppStore) 
-    return  AppStore.bulkCreate(values)};
+    return  AppStore.bulkCreate(values)
+};
 
 export const BulkInsertOS = async (values)=>{
-    values = await filterRepeatedValues(values,'os', OperativeSystem) 
-    return  OperativeSystem.bulkCreate(values)};
+    values = await filterRepeatedValues(values,'os', OperativeSystem)
+    return  OperativeSystem.bulkCreate(values)
+};
 
 export const BulkInsertDeviceID = async (values)=>{
     values = await filterRepeatedValues(values,'deviceid', DeviceId) 
-    return  DeviceId.bulkCreate(values)};
+    return  DeviceId.bulkCreate(values)
+};
 
 export const BulkInsertUserAgent = async (values)=>{
     values = await filterRepeatedValues(values,'ua', UserAgent) 
-    return  UserAgent.bulkCreate(values)};
+    return  UserAgent.bulkCreate(values)
+};
 
 export const BulkInsertUserIP = async (values)=>{
     values = await filterRepeatedValues(values, 'uip', UserIP)
@@ -41,8 +47,25 @@ export const SelectAll = async (attributes, model) => {
     return model.findAll()
 };
 
+const InsertToModel = async (value, model) => {
+    return model.create(value)
+}
 export const saveBundles = async (data) => {
     const {os, bundles, stores, names} = data
+    const insertStores = async (OSValues) => {       
+        const storesWithFK = OSValues.map(s => associateForeignKey(stores, 'os', s, 'OperativeSystemId')).flat()
+        await BulkInsertAppStore(storesWithFK)
+        return (await AppStore.findAll({attributes: ['id','store']})).map(v => v.dataValues)
+    }
+    const insertNames = async (storesValues) => {        
+        const namesWithFK = storesValues.map(s => associateForeignKey(names, 'store', s, 'AppStoreId')).flat()
+        await BulkInsertAppNames(namesWithFK)
+        return (await AppName.findAll({attributes: ['id','name']})).map(v => v.dataValues)
+    }
+    const insertBundles = async (namesValues) => {        
+        const bundlesWithFK = namesValues.map(s => associateForeignKey(bundles, 'name', s, 'AppNameId')).flat()
+        return BulkInsertAppBundle(bundlesWithFK)
+    }
     const associateForeignKey = (tableValues, tableColumn, foreignTableValue, foreignKey) => {
         return tableValues.map(tv => {
             if( tv[tableColumn] == foreignTableValue[tableColumn] ){
@@ -53,23 +76,15 @@ export const saveBundles = async (data) => {
         }).filter(v => v);
     }
     const OSSet = [...new Set(os)]
-    OSSet.map(o => saveIfNotExists(OperativeSystem, {os: o}))
-
+    await BulkInsertOS(OSSet.map(o => {return {os:o}}))
     const OSValues = (await OperativeSystem.findAll({attributes: ['id','os']})).map(v => v.dataValues)
-    const storesWithFK = OSValues.map(s => associateForeignKey(stores, 'os', s, 'OperativeSystemId')).flat()
-    storesWithFK.map(sfk => saveIfNotExists(AppStore, sfk))
-    
-    const storesValues = (await AppStore.findAll({attributes: ['id','store']})).map(v => v.dataValues)
-    const namesWithFK = storesValues.map(s => associateForeignKey(names, 'store', s, 'AppStoreId')).flat() //one-to-many relationship btw store -> name
-    namesWithFK.map(nfk => saveIfNotExists(AppName, nfk))
-
-    const namesValues = (await AppName.findAll({attributes: ['id','name']})).map(v => v.dataValues)
-    const bundlesWithFK = namesValues.map(s => associateForeignKey(bundles, 'name', s, 'AppNameId')).flat()
-    bundlesWithFK.map(bfk => saveIfNotExists(AppBundle, bfk))
+    const storesValues = await insertStores(OSValues)
+    const namesValues = await insertNames(storesValues)
+    await insertBundles(namesValues)
 }
 const saveIfNotExists = async (model, value) => {
     const result = await model.findOne({where : value})
     if(!result) {
-        return model.create(value)
+        return InsertToModel(value, model)
     }
 }
