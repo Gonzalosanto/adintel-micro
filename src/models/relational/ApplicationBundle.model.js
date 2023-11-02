54b0f25f1264832dc1eced9d211db778af5ef76f
gonzalosanto <sancruzgon@gmail.com>
2023-11-02 15:06:48 -0300

    Deleted unnecesary files

--- src/models/relational/ApplicationBundle.model.js
+++
@@ -1,7 +0,0 @@
-import { DataTypes } from "sequelize";
-import { db_client } from '../../db/mariadb.js'
-
-export const AppBundle = db_client.define('AppBundle', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    bundle: {type: DataTypes.STRING, allowNull: false }
-});
--- src/models/relational/UserIP.model.js
+++
@@ -1,7 +0,0 @@
-import { DataTypes } from "sequelize";
-import { db_client } from '../../db/mariadb.js'
-
-export const UserIP = db_client.define('UserIP', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    uip: {type: DataTypes.STRING, allowNull: false }
-});
