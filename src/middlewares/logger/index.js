74f15e0d9f73b52061933aa5ae2465f8428cb93a
gonzalosanto <sancruzgon@gmail.com>
2023-11-02 15:09:09 -0300

    format change

--- src/middlewares/logger/index.js
+++ src/middlewares/logger/index.js
@@ -1,19 +1,15 @@
 export const error = (e) => {
     console.error(`\x1b[31m${e}\x1b[0m`);
 }
-
 export const warning = (w) => {
     console.warn(`\x1b[33m ${w}\x1b[0m`);
 }
-
 export const debug = (d) => {
     console.debug(`\x1b[32m${d}\x1b[0m`)
 }
-
 export const log = (d) => {
     console.log(d)
 }
-
 export const info = (i) => {
     console.info(`\x1b[37m${i}\x1b[0m`);
 }
