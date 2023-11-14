a098da36e6ef115a66d4004f53d7505f50b250b6
gonzalosanto <sancruzgon@gmail.com>
2023-11-14 14:58:28 -0300

    OK: producer definition

--- src/services/kafka/producer.js
+++ src/services/kafka/producer.js
@@ -3,7 +3,40 @@ import { kafka } from "./config.js";
 
 const producer = kafka.producer();
 
-export const handleReportsProduced = async (topic, messages) => {
-    await producer.connect();
-    await producer.send({topic: topic, messages: messages || [{value: 'undefined message'}], compression: CompressionTypes.GZIP})
+export class ProducerFactory {
+    producer;
+    static instance;
+    _isConnected = false;
+    constructor() {
+        this.producer = producer;
+    }
+
+    static getInstance(){
+        if(!ProducerFactory.instance){
+            ProducerFactory.instance = new ProducerFactory()
+        }
+        return ProducerFactory.instance;
+    }
+
+    isConnected(){
+        return this._isConnected;
+    }
+
+    async start() {
+        try {
+            await this.producer.connect()
+            this._isConnected = true;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async shutdown(){
+        await this.producer.disconnect()
+        this._isConnected = false;
+    }
+
+    async sendMessages(topic, messages){
+        await this.producer.send({ topic: topic, messages: messages || [{ value: 'undefined message' }], compression: CompressionTypes.GZIP })
+    }
 }
