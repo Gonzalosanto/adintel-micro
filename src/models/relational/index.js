4617310338fecd4294cd90eb51406e5a8b34f889
gonzalosanto <sancruzgon@gmail.com>
2023-12-14 19:13:45 -0300

    UPDATE

--- src/models/relational/index.js
+++
@@ -1,4 +0,0 @@
-import { Sequelize } from "sequelize";
-const db_name = 'VAST_DB'
-
-export const client = new Sequelize(`mariadb://localhost:/${db_name}`)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { abort, start } from "./requests/index.js";
-import { saveFileData } from "./requests/file.js";
 
 export const router = Router();
 
@@ -15,12 +14,4 @@ router.get('/start', async(req,res, next) => {
 router.get('/abort', async(req,res, next) => {
     res.status(200).send("Killing process...")
     abort(req,res,next)
-})
-router.get('/file', async (req,res, next)=> {
-    try {
-        saveFileData()
-        res.status(200).send("Your request has been fulfilled")
-    } catch (error) {
-        res.status(500).send(error)
-    }
 })
