c1e3839480f805a4b92409845e66673cff26f91c
gonzalosanto <sancruzgon@gmail.com>
2023-12-27 19:59:25 -0300

    new class to handle reqs as prom's queue

--- src/services/kafka/producer.js
+++ src/services/kafka/producer.js
@@ -1,42 +1,9 @@
 import { CompressionTypes } from "kafkajs";
 import { kafka } from "./config.js";
 
-const producer = kafka.producer();
+const producer = kafka.producer({allowAutoTopicCreation: true});
 
-export class ProducerFactory {
-    producer;
-    static instance;
-    _isConnected = false;
-    constructor() {
-        this.producer = producer;
-    }
-
-    static getInstance(){
-        if(!ProducerFactory.instance){
-            ProducerFactory.instance = new ProducerFactory()
-        }
-        return ProducerFactory.instance;
-    }
-
-    isConnected(){
-        return this._isConnected;
-    }
-
-    async start() {
-        try {
-            await this.producer.connect()
-            this._isConnected = true;
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async shutdown(){
-        await this.producer.disconnect()
-        this._isConnected = false;
-    }
-
-    async sendMessages(topic, messages){
-        await this.producer.send({ topic: topic, messages: messages || [{ value: 'undefined message' }], compression: CompressionTypes.GZIP })
-    }
+export const sendReport = async (message) => {
+    await producer.connect();
+    await producer.send({topic: process.env.REPORTS_TOPIC, messages: message, compression: CompressionTypes.GZIP});
 }
